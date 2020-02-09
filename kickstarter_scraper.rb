3b72925f7c2a42e2a5ff4cf771674b9b09fcfa27
Erica Schimmel <ericaschimmel01@gmail.com>
2020-02-09 02:35:33 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -36,7 +36,9 @@ def create_project_hash
       
       :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
       
-      :description => 
+      :description => project.css("p.bbcard_blurb").text,
+      
+      
       
       
       
