1790c60b66ba9006f4117d6ad7962b13d00b5f1f
Erica Schimmel <ericaschimmel01@gmail.com>
2020-02-09 02:30:27 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -9,8 +9,13 @@ def create_project_hash
   
   kickstarter = Nokogiri::HTML(html)
   
+  projects = {} 
   
+  kickstarter.css("li.project.grid_4").each do |project|
+    projects[project] = {}
+  end 
   
+  projects 
 end
 
 create_project_hash
